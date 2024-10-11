ae360480175a2b1c76a5f670eacdb72ef5872bba
Luke Davis <lpdavis13@gmail.com>
2024-10-11 09:58:05 -0400

    Initial Commit

---
+++ FeastIdeas/FeastIdeasApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeastIdeasApp.swift
+//  FeastIdeas
+//
+//  Created by Luke Davis on 10/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FeastIdeasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
